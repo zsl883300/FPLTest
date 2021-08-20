dbc2c29b5c7e1132113e877cba340349277a12d1
zsl github <zsldqs883300@126.com>
2021-08-20 15:26:47 +0800

    123

--- Frameworks/Livehelp.framework/Headers/Livehelp.h
+++
@@ -1,7 +0,0 @@
-
-
-#import <Foundation/Foundation.h>
-#import <Livehelp/LivehelpSupport.h>
-#import <Livehelp/LivehelpSupport+Interface.h>
-#import <Livehelp/LivehelpSupportNavigationController.h>
-
